6413b6116ad527f8ff7a8a11d9aad23c922f2044
soha-sultana <sohas@uw.edu>
2025-10-26 16:55:31 -0700

    Using sketch 6 template

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -1,14 +1,30 @@
 // Instance-mode sketch for tab 4
 registerSketch('sk4', function (p) {
-  p.setup = function () {
+p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    // clockFont = p.loadFont("digital-7.ttf");
   };
+
   p.draw = function () {
-    p.background(200, 240, 200);
-    p.fill(30, 120, 40);
-    p.textSize(32);
+    p.background(0);
+    p.clock();
+  }
+
+  p.clock = function () {
+    let hr = p.hour();
+    let mn = p.minute();
+    let sc = p.second();
+
+    p.fill("lavender");
+    // p.textFont(clockFont);
     p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. C', p.width / 2, p.height / 2);
-  };
+    p.textSize(p.width / 10);
+    let noon = hr >= 12 ? " PM" : " AM"
+    if (mn < 10)
+      mn = "0" + mn
+    hr %= 12
+    p.text(hr + ":" + mn + ":" + sc + noon, p.width / 2, p.height / 2);
+
+  }
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
 });
