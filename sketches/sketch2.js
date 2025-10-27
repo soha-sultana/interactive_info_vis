3066b46fe821249f5f5c935018e62383751622e3
soha-sultana <sohas@uw.edu>
2025-10-26 19:03:34 -0700

    Add files via upload

---
+++ sketches/sketch2.js
@@ -0,0 +1,98 @@
+// Instance-mode sketch for tab 2
+registerSketch('sk2', function (p) {
+  let sectionHeight;
+  p.setup = function () {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    sectionHeight = p.height / 3;
+  };
+  p.draw = function () {
+    p.background(220);
+
+    // background color for first section
+    p.noStroke();
+    p.fill(256,200,190);
+    p.rect(0, 0, p.width, sectionHeight);
+
+    // background color for second section
+    p.noStroke();
+    p.fill(256,220,190);
+    p.rect(0, sectionHeight, p.width, sectionHeight);
+
+    // adding the section dividers (into 3 parts)
+    p.stroke(180);
+    p.strokeWeight(1);
+    p.line(0, sectionHeight, p.width, sectionHeight);
+    p.line(0, sectionHeight * 2, p.width, sectionHeight * 2);
+
+    // First Section
+    p.noStroke();
+    p.fill(30);
+    p.textAlign(p.LEFT, p.TOP);
+    p.textSize(80);
+    p.text('Class 1: History',50,50);
+
+    p.textSize(50);
+    const currentDate = new Date();
+    p.text(currentDate.toDateString(),50,150)
+
+    p.text('Assignment 2', 50,210);
+
+    // Second Section
+    // initialize time variables
+    const now = new Date();
+    const start = now.toLocaleTimeString();
+    const endDate = new Date(now.getTime() + 60 * 60 * 1000); 
+    const endTime = endDate.toLocaleTimeString();
+
+    // add position variables 
+    const midTop = sectionHeight;
+    const midCenterY = midTop + sectionHeight / 2;
+
+    // Left side (Start Time)
+    p.noStroke();
+    p.fill(30);
+    p.textAlign(p.LEFT, p.CENTER);
+    p.textSize(40);
+    p.text('Current Time:'+ start, 50, midCenterY - 6);
+    p.textAlign(p.LEFT, p.TOP);
+
+    // Right side (End time)
+    p.textAlign(p.RIGHT, p.CENTER);
+    p.text('End Time:'+ endTime, p.width-50, midCenterY - 5);
+
+    // adding circle in the center (to act as a button)
+    p.stroke(40);
+    p.fill(220, 120, 180)
+    p.ellipse(p.width / 2, midCenterY, 100, 100);
+
+    // adding a square in the circle to act as a button 
+    p.noStroke();
+    p.fill(0);
+    p.rectMode(p.CENTER);
+    p.rect(p.width / 2, midCenterY, 40, 40);
+    p.rectMode(p.CORNER);
+
+    // Third section
+    p.noStroke();
+    p.fill(190,256,220);
+    p.rect(0, sectionHeight * 2, p.width, sectionHeight);
+
+    // Resources text
+    p.fill(30);
+    p.textAlign(p.LEFT, p.TOP);
+    p.textSize(80);
+    p.text('Resources:', 50, sectionHeight * 2 + 50);
+    p.textSize(20);
+    p.text('- Textbook\n- Lecture Slides\n- Online Articles\n- Tutorials\n- Documentation', 50, sectionHeight * 2 + 150);
+  
+
+
+  //  p.fill(100, 150, 240);
+  //  p.textSize(32);
+  //  p.textAlign(p.CENTER, p.CENTER);
+  //  p.text('HWK #4. A', p.width / 2, p.height / 2);
+  };
+  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
+});
+    
+
